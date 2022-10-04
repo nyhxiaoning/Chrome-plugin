5efcb0b0a5ea28d516b4e2d4374a242b8f08ccc7
daning <1540097293@qq.com>
2022-10-05 00:54:31 +0800

    base-chrome-cli-plugin-vue

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,14 +1,15 @@
 /*
  * @Author: migor
  * @Date: 2021-11-25 20:54:11
- * @LastEditors: migor
- * @LastEditTime: 2021-11-26 10:21:45
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-10-05 00:50:26
  * @Description: file content
  */
 import { createApp } from 'vue'
 import app from './components/app.vue'
 
-import AltihexDesignVue from "altihex-design"
-import 'altihex-design/dist/altihex.css'
+// import AltihexDesignVue from "altihex-design"
+// import 'altihex-design/dist/altihex.css'
 
-createApp(app).use(AltihexDesignVue).mount('#app')
+// createApp(app).use(AltihexDesignVue).mount('#app')
+createApp(app).mount('#app')
